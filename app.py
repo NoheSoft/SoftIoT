5921ace841299ba8e9fa83adec14e1f23a92829d
NoheSoft <alaizaveraronquillo5@gmail.com>
2025-06-15 13:05:15 -1200

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    print("Iniciando servidor Flask en http://localhost:8081mmmmmmmmmmmmmmmm...")
+    print("Iniciando servidor Flask en http://localhost:8081...")
     app.run(host='0.0.0.0', port=8081, debug=True)
